3e2461b6439e3b8960c2f4fed01f2ee069b4c105
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2021-06-05 16:16:54 +0400

    ***

---
+++ .NET Core/Reports/Reports/Models/DBCtx.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Reports.Models
+{
+    public class DBCtx : DbContext
+    {
+        public DBCtx(DbContextOptions<DBCtx> options) : base(options)
+        {
+            //Disable "Lazy Loading"
+            //ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
