44b727bea504dae5c28338dbec8ea6d637068cba
Moayadooh <49630526+Moayadooh@users.noreply.github.com>
2023-09-13 15:41:26 +0400

    Add .NET 6 Project

---
+++ .NET Core/BookLibrary/BookLibrary/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookLibrary.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //Disable "Lazy Loading"
+            //ChangeTracker.LazyLoadingEnabled = false;
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ .NET Core/BookLibrary/BookLibrary/Models/Book.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BookLibrary.Models
+{
+    public class Book
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [Required]
+        [DisplayName("Book Name")]
+        public string? Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter release date")]
+        [DisplayName("Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        public DateTime ReleaseDate { get; set; } = DateTime.Now;
+
+        [Required]
+        [DisplayName("Display Order")]
+        [Range(1, int.MaxValue, ErrorMessage = "Display Order for category must be greater than 0")]
+        public long DisplayOrder { get; set; }
+
+        //[Required]
+        //[EmailAddress]
+        //[RegularExpression(@"[a-zA-Z0-9_\.+]+@(live|hotmail|gmail)(\.com|\.om)", ErrorMessage = "Must be live, hotmail or gmail")]
+        //[MaxLength(50)]
+        //public string? Email { get; set; }
+    }
+}
